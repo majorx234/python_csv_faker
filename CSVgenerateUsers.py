de642bcf181a953fe0f72011d7e7e27562ddac30
Major&Hannah <majorx234@googlemail.com>
2021-02-07 19:02:27 +0100

    add md5 in csv instead of a password

--- CSVgenerateUsers.py
+++ CSVgenerateUsers.py
@@ -10,13 +10,14 @@ from faker import Faker
 import datetime
 from games_provider import GamesProvider
 from passwords_provider import PasswordsProvider
+import hashlib
 
 def datagenerate(records, headers):
     fake = Faker('en_US')
 
     fake.add_provider(PasswordsProvider)
 
-    with open("users_data2.csv", 'wt') as csvFile:
+    with open("users_data3.csv", 'wt') as csvFile:
         writer = csv.DictWriter(csvFile, fieldnames=headers)
         writer.writeheader()
         for i in range(records):
@@ -28,14 +29,19 @@ def datagenerate(records, headers):
             domain_name = "@testDomain.com"
             email = Fname +"."+ Lname + domain_name
             username = Fname + "X234"
+            password = fake.passwords()
+            premd5password = password.encode()
+            md5password = hashlib.md5(premd5password)
+            md5password_digest = md5password.hexdigest()
             writer.writerow({
                 "UserId" : userId,
                 "FirstName" : Fname,
                 "LastName": Lname,
                 "UserName": username,
-                "Password": fake.passwords(),
+                "Password":  md5password_digest,
                 "Email": email
             })
+            print("user: " + username + " password: " + password + " hash: " + md5password_digest )   
     
 if __name__ == '__main__':
     records = 10
